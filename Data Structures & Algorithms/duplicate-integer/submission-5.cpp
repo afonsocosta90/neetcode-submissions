10a136009db9776f4e3663e8bdc1fe8c909723cd
afonsocosta90 <afonsocosta90@gmail.com>
2026-05-25 15:28:47 +0200

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        if (nums.empty()) return false;
+        std::sort(nums.begin(), nums.end());
+        int duplicate_contender = nums[0];
+        int n = std::size(nums);
+
+        for (int i = 1; i < n; i++){
+            if (nums[i] == duplicate_contender){
+                return true;
+            }
+            duplicate_contender = nums[i];
+        }
+        
+        return false;
+    }
+};

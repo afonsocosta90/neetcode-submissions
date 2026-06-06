cf69cabcc5a8ce6fa8cb71e688ff01b8a09e9ed7
Afonso Costa <afonsocosta90@gmail.com>
2026-06-06 22:45:56 +0200

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,24 @@
+class MinStack {
+public:
+    MinStack() {}
+    void push(int val) {
+        s.push(val);
+        if (mins.empty() || val <= mins.top())
+            mins.push(val);
+        else
+            mins.push(mins.top());
+    }
+    void pop() {
+        s.pop();
+        mins.pop();
+    }
+    int top() {
+        return s.top();
+    }
+    int getMin() {
+        return mins.top();
+    }
+private:
+    std::stack<int> s;
+    std::stack<int> mins;
+};
